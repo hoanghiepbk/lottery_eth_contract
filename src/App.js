02b8f82ab1ff386246eee94a90813fc8d5293b06
hoanghiep <hoanghiepphambk@gmail.com>
2022-04-02 11:11:24 +0700

    eth-smart-contract

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+class App extends React.Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: "",
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+    this.setState({ message: "You have been entered!" });
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+    this.setState({ message: "A winner has been picked!" });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          Rule is simple, the web auto connect to your MetaMask wallet, anyone can send their ether, a random winner will take them all !
+        </p>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label><br></br>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button className="button">Enter</button>
+        </form>
+
+        <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button> <span>(Only manager can run)</span>
+
+        <hr />
+
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+export default App;
